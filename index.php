52e31f0f35e73ab6ab80eade3487ea0a29a4e483
manfred-commits <manfredipiraino@gmail.com>
2021-10-18 15:01:59 +0200

    var_dump test on GET

--- index.php
+++ index.php
@@ -1,11 +1,13 @@
 <!-- nome repo: php-badwords
 Descrizione:
-Creare una variabile con un paragrafo di testo a vostra scelta.
-Stampare a schermo il paragrafo e la sua lunghezza.
+Creare una variabile con un paragrafo di testo a vostra scelta. ok
+Stampare a schermo il paragrafo e la sua lunghezza. ok
 Una parola da censurare viene passata dall'utente tramite parametro GET.
 Stampare di nuovo il paragrafo e la sua lunghezza, dopo aver sostituito con tre asterischi (***) tutte le occorrenze della parola da censurare. -->
 <?php 
 $paragraph= "Il nostro compito è di fare il possibile per la salvezza degli anni nei quali viviamo, sradicando il male dai campi che conosciamo, al fine di lasciare a coloro che verranno dopo terra sana e pulita da coltivare. Ma il tempo che avranno non dipende da noi.";
+$userInput=$GET['input'];
+var_dump($_GET);die;
 ?>
 
 <!DOCTYPE html>
@@ -17,8 +19,15 @@ $paragraph= "Il nostro compito è di fare il possibile per la salvezza degli ann
     <title>PHP Badwords</title>
 </head>
 <body>
-    <h1>Paragrafo</h1>
+    <h2>Paragrafo</h2>
     <p><?php echo $paragraph;?> </p>
+    <p>Lunguezza paragrafo: <?php echo strlen($paragraph);?></p>
+    <hr>
+    <h2>Paragrafo</h2>
+    <p>
+
+        <!-- <?php echo str_replace('userInput','(***)',$paragraph);?> -->
+    </p>
     <p>Lunguezza paragrafo: <?php echo strlen($paragraph);?></p>
 </body>
 </html>
